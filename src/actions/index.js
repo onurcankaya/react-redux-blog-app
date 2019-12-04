327c8bb5ba6641cdeb8139e06d02738c98640c9d
onurcankaya <onur.eren.cankaya@gmail.com>
2019-12-04 13:37:58 +0100

    implemented memoization for the fetchUser async action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { memoize } from 'lodash'
 import fetch from '../services/api'
 
 export const fetchPosts = () => async (dispatch) => {
@@ -7,7 +8,11 @@ export const fetchPosts = () => async (dispatch) => {
 }
 
 export const fetchUser = (id) => async (dispatch) => {
+  _fetchUser(id, dispatch)
+}
+
+const _fetchUser = memoize(async (id, dispatch) => {
   const response = await fetch.get(`/users/${id}`)
 
   dispatch({ type: 'FETCH_USER', payload: response.data })
-}
+})
