081951d91dd86e9d9825e5f76fa54543386a9db3
onurcankaya <onur.eren.cankaya@gmail.com>
2019-12-04 14:37:23 +0100

    removed unused async keyword

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
   _fetchUser(id, dispatch)
 }
 
